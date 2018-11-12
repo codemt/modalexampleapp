28a6f02b3c4412b9053a0ac5005ec6c5a922a7c2
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-11-12 23:53:18 +0530

    added theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Modal from './components/modal/index';
+import NavBar from './components/navbar';
 
 class App extends Component {
   render() {
     return (
+      <div>
 
-      <Modal />
+
+              <NavBar />
+              <Modal />
+
+
+              
+      </div>
+    
+
     );
   }
 }
